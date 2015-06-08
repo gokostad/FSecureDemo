10189f87af868e44629fbd89f54deb9d1dbf70ae
Goran Kostadinov <kostadinov.goran@gmail.com>
2015-06-08 16:33:16 +0300

    Adding selected menu action.

--- app/src/main/java/com/gorankostadinov/fsecure/interviewdemo/SongListActivity.java
+++ app/src/main/java/com/gorankostadinov/fsecure/interviewdemo/SongListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 
@@ -33,13 +34,6 @@ public class SongListActivity extends ActionBarActivity
      */
     private boolean mTwoPane;
     private ListView list;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,5 +82,27 @@ public class SongListActivity extends ActionBarActivity
             detailIntent.putExtra(SongDetailFragment.ARG_ITEM_ID, id.toString());
             startActivity(detailIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
